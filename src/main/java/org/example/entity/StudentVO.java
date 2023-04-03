3d6a40e51b1768a47d10da1604d04d1750e31fe0
shreyash <shreyasgaikwad52@gmail.com>
2023-04-03 21:30:59 +0530

    Add

---
+++ src/main/java/org/example/entity/StudentVO.java
@@ -0,0 +1,41 @@
+package org.example.entity;
+
+public class StudentVO {
+	
+	int rollno;
+	String fname;
+	String age;
+	String mobno;
+
+	public int getRollno() {
+		return rollno;
+	}
+
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getMobno() {
+		return mobno;
+	}
+
+	public void setMobno(String mobno) {
+		this.mobno = mobno;
+	}
+}
